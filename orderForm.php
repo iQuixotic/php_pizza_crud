891e6edcda2bdb7e7b3e884d9e6af2cd3597ec18
Trey <treyw1850@gmail.com>
2018-09-27 12:22:27 -0400

    set up basic styling for order page

---
+++ orderForm.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="views/assets/css/base.css" />   
+    <link rel="stylesheet" type="text/css" media="screen" href="views/assets/css/toppings.css" />    
+</head>
+<body>
+    <div class="container">
+    <form action="Insert">
+    <div class="form-header">Pizza Name:</div>  <br> <input type="text" Name="pizza-name" value=""><br>
+    <div class="form-header">Customer:</div><br>  <input type="text" Name="customer: " value=""><br>
+    <div class="form-header">Toppings:</div>  <br> <input type="text" Name="toppings: " value=""><br>
+
+    <div class="Toppings">
+        <div class="Toppings_single">
+            <h5>Mushroom</h5>
+            <img src="./views/assets/img/mushroom.jpg" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>            
+        </div>
+        <div class="Toppings_single">
+            <h5>Pepperoni</h5>
+            <img src="./views/assets/img/peps.jpg" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+        <div class="Toppings_single">
+            <h5>Tomato</h5>
+            <img src="./views/assets/img/tomato.jpg" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+        <div class="Toppings_single">
+            <h5>Bell Pepper (Green)</h5>
+            <img src="./views/assets/img/greenPepper.jpg" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+        <div class="Toppings_single">
+            <h5>Bell Pepper (Red)</h5>
+            <img src="./views/assets/img/redPepper.jpg" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+        <div class="Toppings_single">
+            <h5>Ham</h5>
+            <img src="./views/assets/img/ham.jpg" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+        <div class="Toppings_single">
+            <h5>Mozzarella</h5>
+            <img src="./views/assets/img/mozz.jpg" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+        <div class="Toppings_single">
+            <h5>Olive</h5>
+            <img src="./views/assets/img/olive.png" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+        <div class="Toppings_single">
+            <h5>Onion</h5>
+            <img src="./views/assets/img/onion.jpg" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+        <div class="Toppings_single">
+            <h5>Pineapple</h5>
+            <img src="./views/assets/img/pineapple.jpg" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+        <div class="Toppings_single">
+            <h5>Provolone</h5>
+            <img src="./views/assets/img/provolone.png" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+        <div class="Toppings_single">
+            <h5>Spinach</h5>
+            <img src="./views/assets/img/spinach.jpg" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+        <div class="Toppings_single">
+            <h5>Tomato</h5>
+            <img src="./views/assets/img/tomato.jpg" alt="">
+            <div class="block">
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">Yep</span>
+                <input class ='Toppings_checkbox' type="checkbox"> 
+                <span class="Toppings_checkbox-txt">2x</span>
+            </div>
+        </div>
+    
+    
+
+    </div>
+    
+
+    
+    <div class="form-header">Price:</div>  <br> <input type="text" Name="price: " value=""><br>
+    </form>
+    </div>
+    
+    
+</body>
+</html>
